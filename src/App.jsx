d00c4683e74eec9ae224f60ea759f3dae521d4b7
sahil gupta <8278087870sahil@gmail.com>
2024-01-15 14:09:24 +0530

    style:bg color change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="relative z-0 bg-primary">
+      <div className="relative z-0 bg-secondary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
