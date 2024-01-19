709cc398360af7e1cf69b3e2af303586c29113da
sahil gupta <8278087870sahil@gmail.com>
2024-01-20 00:22:41 +0530

    feat: projects link in navbar.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -93,4 +93,4 @@ const Works = () => {
   );
 };
 
-export default SectionWrapper(Works, "work");
+export default SectionWrapper(Works, "projects");
--- src/constants/index.js
+++ src/constants/index.js
@@ -36,6 +36,10 @@ export const navLinks = [
     title: "Work",
   },
   {
+    id: "projects",
+    title: "Projects",
+  },
+  {
     id: "contact",
     title: "Contact",
   },
