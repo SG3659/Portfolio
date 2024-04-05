922213ca2e58f3aef28f0e4de1c87cc2b059037e
sahil gupta <8278087870sahil@gmail>
2024-04-05 19:07:01 +0530

    docs:update resume

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import resume from "./Sahil Gupta-Resume.pdf";
+import resume from "./SahilGupta__Resume.pdf";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
