6fdfb797fb05fb935212d118d82e9ba2ae7a59ff
Sahil Gupta <102417163+SG3659@users.noreply.github.com>
2024-08-06 10:37:20 +0530

    Update SectionWrapper.jsx

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,7 +10,7 @@ const SectionWrapper = (Component, idName) =>
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         {/*it used for scroll the button */}
         <span className="hash-span" id={idName}>
